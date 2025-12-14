170f56e78f26f057b93e40935cd5d5a5f07af8ef
Meitar Shimoni <141142200+MeitarShimoni@users.noreply.github.com>
2025-12-14 22:35:07 +0200

    Refactor timer_sva with additional properties

--- verification/timer_sva.sv
+++ verification/timer_sva.sv
@@ -30,23 +30,36 @@ module timer_sva(
         (req && !gnt) |-> ##1 req; // if req 1 and gnt 0. req has to stay high. 
     endproperty
     
-    property reset_clears_gnt_p;
+    property R4_reset_clears_gnt_p;
         @(posedge clk) 
         !reset_n |=> !gnt;
     endproperty
 
+    property R5_GNT_CLEARED_AFTER_REQ_LOW;
+        @(posedge clk) disable iff (!reset_n)
+        $fell(req) |-> !gnt;
+    endproperty
+
+    property R6_UNMAPPED_READ_RETURNS_ZERO;
+        @(posedge clk) disable iff (!reset_n)
+        (addr != P_ADDR_LOAD && addr != P_ADDR_CONTROL && addr != P_ADDR_STATUS && !write_en) |-> (rdata == 0);
+    endproperty
+
 
     R1: assert property(R1_GNT_ASSERT_3_CYCLES)
-        else begin
-            $error("SVA R1 FAILED! GNT did not go high within 3 cycles after REQ!");
-        end
-
+        else $error("SVA R1 FAILED! GNT did not go high within 3 cycles after REQ!");
+        
     R2: assert property(R2_REQ_WAIT_2_GNT)
-        else begin
-            $error("SVA R2 FAILED! REQ did not stay high until GNT was high!");
-        end
-
-    R4: assert property (reset_clears_gnt_p)
+        else $error("SVA R2 FAILED! REQ did not stay high until GNT was high!");
+        
+    R4: assert property (R4_reset_clears_gnt_p)
         else $error("SVA R4 FAILED! GNT is not 0 immediately after reset!");
+
+    R5: assert property (R5_GNT_CLEARED_AFTER_REQ_LOW)
+        else $error("SVA R5 FAILED! GNT did not go low after REQ went low!");
+
+    R6: assert property (R6_UNMAPPED_READ_RETURNS_ZERO)
+        else $error("SVA R6 FAILED! Unmapped read did not return zero!");
+
 
 endmodule : timer_sva
